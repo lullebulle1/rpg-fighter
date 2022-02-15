bb5735f8d620538645887b581732d5f5b24ac95c
lullebulle2 <lvmd04@gmail.com>
2022-02-15 14:34:50 +0100

    added file

---
+++ main.py
@@ -0,0 +1,11 @@
+
+from resources import Character, Goblin
+
+
+if __name__=="main":
+    nemy = Character("namy",20,5,3)
+    goblin_one = Goblin(10,3,1)
+
+    print(nemy)
+    print()
+    print(goblin_one)
