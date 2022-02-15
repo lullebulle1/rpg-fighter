cc143e41c58f9e0dfbbda2c7777443b569362a3f
lullebulle2 <lvmd04@gmail.com>
2022-02-15 14:43:27 +0100

    added fighting methodes

--- resources.py
+++ resources.py
@@ -7,14 +7,38 @@ class Character:
         self.armor=armor
     
     def __str__(self) -> str:
-        return f"Name: {self.name}\nhealth {self.health}\n attack {self.attack}\n armor {self.armor}"
+        return f"Name: {self.name}\nhealth {self.health}\nattack {self.attack}\narmor {self.armor}"
+    
+    def attack(self):
+        return self.attack
+    
+    def take_damage(self, damage):
+        relative_damage = damage-self.armor
+        self.health -= relative_damage
+        if self.health < 0: self.health = 0
+    
+    def get_health(self):
+        return self.health
 
 class Goblin:
-    def __init__(self, health, attack, armor) -> None:
+    def __init__(self, health, attack, armor):
         self.health=health
         self.attack=attack
         self.armor=armor
     
     def __str__(self) -> str:
-        return f"Goblin\nhealth {self.health}\narmor {self.armor}"
+        return f"Goblin\nhealth {self.health}\nattack {self.attack}\narmor {self.armor}"
+
+    def attack(self):
+        return self.attack
+    
+    def take_damage(self, damage):
+        relative_damage = damage-self.armor
+        self.health -= relative_damage
+        if self.health < 0: self.health = 0
+    
+    def get_health(self):
+        return self.health
+
+
         
